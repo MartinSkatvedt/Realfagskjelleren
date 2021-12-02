0884bd375be75c7396f1034c7138455991f91820
Martin Skatvedt <martinskatvedt@gmail.com>
2021-12-02 22:38:00 +0100

    Update settings.py

--- rfkBackend/rfkBackend/settings.py
+++ rfkBackend/rfkBackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3uo(8e)1+mt5!v48t23im718$@lqbxkre0r5mi(d0mn^c8c+5m'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
