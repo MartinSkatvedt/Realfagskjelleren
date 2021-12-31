4176bf1926757ecf020cae8482d315b979cb3a7e
Martinskatvedt <martinskatvedt@gmail.com>
2021-12-31 04:17:50 +0100

    update wsgi

--- rfkBackend/rfkBackend/wsgi.py
+++ rfkBackend/rfkBackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rfkBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rfkbackend.rfkBackend.settings')
 
 application = get_wsgi_application()
