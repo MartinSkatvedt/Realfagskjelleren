5bbfbaa40eee11f597794f4d89ad5502d6549289
Martinskatvedt <martinskatvedt@gmail.com>
2022-01-01 23:26:31 +0100

    made views protected

--- rfkBackend/inventoryManager/views.py
+++ rfkBackend/inventoryManager/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 # Create your views here.
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ProductSerializer, MerchSerializer
 from .models import Product, Merch
 
+
 class ProductView(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
+
 class MerchView(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = MerchSerializer
     queryset = Merch.objects.all()
